ad6a30cc46d673e51c171891c74e191b6fa19932
Omachi John <omachi.john@.com>
2022-08-24 23:21:54 +0100

    Add render to dom module

--- src/modules/renderList.js
+++ src/modules/renderList.js
@@ -1,26 +1,26 @@
-// import id from './idGames';
-// const postUrl =
-//   'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+import id from './idGames';
+const postUrl =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
 
-// class UI {
-//   static getData = async () => {
-//     const res = await fetch(`${postUrl}/${id}/scores`);
-//     const datas = await res.json();
-//     const dataResult = datas.result;
-//     dataResult.forEach((data) => {
-//       UI.renderTodom(data);
-//     });
-//   };
+class UI {
+  static getData = async () => {
+    const res = await fetch(`${postUrl}/${id}/scores`);
+    const datas = await res.json();
+    const dataResult = datas.result;
+    dataResult.forEach((data) => {
+      UI.renderTodom(data);
+    });
+  };
 
-//   static renderTodom(data) {
-//     const tbody = document.getElementById('tbody');
-//     const tableRow = document.createElement('tr');
-//     tableRow.innerHTML = `
-//       <td>${data.user}</td>
-//       <td>${data.score}</td>
-//       `;
+  static renderTodom(data) {
+    const tbody = document.getElementById('tbody');
+    const tableRow = document.createElement('tr');
+    tableRow.innerHTML = `
+      <td>${data.user}</td>
+      <td>${data.score}</td>
+      `;
 
-//     tbody.appendChild(tableRow);
-//   }
-// }
-// export default UI;
+    tbody.appendChild(tableRow);
+  }
+}
+export default UI;
